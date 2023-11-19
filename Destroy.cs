91f5b68c167bf1cec6a13247e933156dfa6aaff9
naydiYTomg <85066344+naydiYTomg@users.noreply.github.com>
2023-11-19 13:24:42 +0300

    Another new file

---
+++ Destroy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject cube;
+    void Start()
+    {
+        Destroy(cube);
+    }
+
+    // Update is called once per frame
+
+}
